2fb47d65ac6b3e0f8a21248c54511b008c52d3de
rraires <rraires@gmail.com>
2021-09-14 18:31:26 -0300

    Add files via upload

--- homol.py
+++ homol.py
@@ -16,6 +16,14 @@ import statsmodels as sm
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
+st.set_page_config(  # Alternate names: setup_page, page, layout
+    layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+    initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
+    page_title=None,  # String or None. Strings get appended with "• Streamlit".
+    page_icon=None,  # String, anything supported by st.image, or None.
+)
+
+
 
 def sazonalidade():
     st.header('Análise de Sazonalidade')
